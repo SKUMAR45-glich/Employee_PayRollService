583dcc93cb2bd30afd9b89c9f8629aebf0ce33e7
Saurabh <saurabh.nit20@gmail.com>
2020-11-08 01:59:17 +0530

    [Saurabh].Add Retrieve Data from Employee PayRoll Service

--- Program.cs
+++ Program.cs
@@ -11,6 +11,21 @@ namespace EmployeePayRollService
             EmployeeRepo repo = new EmployeeRepo();
             repo.GetAllEmployee();
 
+            EmployeeModel employee = new EmployeeModel();
+            employee.name = "Indal";
+            employee.Department = "Tech";
+            employee.phone = "6302907918";
+            employee.address = "02-Khajauli";
+            employee.gender = 'M';
+            employee.salary = 10000.00M;
+            employee.Deduction = 1500.00;
+            employee.taxable_pay = 2500.00;
+            employee.income_tax = 1800.00;
+            employee.net_pay = 1200.00;
+            employee.start = employee.start = Convert.ToDateTime("2020-11-03");
+
+            if (repo.AddEmployee(employee))
+                Console.WriteLine("Records added successfully");
         }
     }
 }
