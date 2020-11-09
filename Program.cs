affe9841e8abb3272b29b7cd22205dcf7125d006
Saurabh <saurabh.nit20@gmail.com>
2020-11-10 01:00:33 +0530

    [Saurabh].Get Aggregate Results for Female Employees

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 
 namespace EmployeePayRollService
 {
@@ -8,10 +9,10 @@ namespace EmployeePayRollService
         {
             Console.WriteLine("Welcome To Employee PayRoll Service!");
 
-            EmployeeRepo repo = new EmployeeRepo();
-            repo.GetAllEmployee();
+            EmployeeRepo repo = new EmployeeRepo();                                             //Declare Object for EmployeeRepo Class
+            repo.GetAllEmployee();                                                              //Get Details for Employee
 
-            EmployeeModel employee = new EmployeeModel();
+            EmployeeModel employee = new EmployeeModel();                                       //Declare Object for EmployeeModel Class
             employee.EmployeeName = "Indal";
             employee.Department = "Tech";
             employee.PhoneNumber = "6302907918";
@@ -24,23 +25,26 @@ namespace EmployeePayRollService
             employee.NetPay = 1200.00;
             employee.StartDate = Convert.ToDateTime("2020-11-03");
 
-            /*if (repo.AddEmployee(employee))
-                Console.WriteLine("Records added successfully");
+            /*if (repo.AddEmployee(employee))                                                        
+                Console.WriteLine("Records added successfully");                                 //Adding Data to Table
             */
 
 
-            Salary salary = new Salary();
+            //Salary salary = new Salary();                                                        // Salary Model
 
-            SalaryUpdateModel salaryUpdateModel = new SalaryUpdateModel { SalaryId = 1, EmployeeSalary = 3000000 };
+            //SalaryUpdateModel salaryUpdateModel = new SalaryUpdateModel { SalaryId = 1, EmployeeSalary = 3000000 };               //Update Salary
 
-            var updatedSalary = salary.UpdateEmployeeSalary(salaryUpdateModel);
+            //var updatedSalary = salary.UpdateEmployeeSalary(salaryUpdateModel);
 
-            Console.WriteLine(updatedSalary);
+            //Console.WriteLine(updatedSalary);
 
-            /*DateTime startDate = Convert.ToDateTime("01/01/2018");
+            /*DateTime startDate = Convert.ToDateTime("01/01/2018");                                    //Get Data in the Range of Data
             DateTime endDate = Convert.ToDateTime("01/01/2019");
 
             repo.GetAllEmployeeInGivenDateRange(startDate, endDate);
-        */}
+        */
+            repo.GetCountAvgMinMaxinEmployee(employee);                                             //Get Females Count with Max Min Data
+        
+        }
     }
 }
